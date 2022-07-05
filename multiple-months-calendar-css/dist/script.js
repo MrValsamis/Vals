77d4e51e4b7cedd9af7789891adbd0d57b0278ad
MrValsamis <85327560+MrValsamis@users.noreply.github.com>
2022-07-06 00:06:35 +0300

    45

--- multiple-months-calendar-css/dist/script.js
+++ multiple-months-calendar-css/dist/script.js
@@ -1,7 +1,7 @@
 // generate events
 var eventDates = {}
 
-let day1 = formatDate(new Date(new Date().setDate(new Date().getDate() + 50 )))
+let day1 = formatDate(new Date(new Date().setDate(new Date().getDate() + 45 )))
 eventDates[day1] = [
   'Daily Agenda</br></br>More text',
 ]
